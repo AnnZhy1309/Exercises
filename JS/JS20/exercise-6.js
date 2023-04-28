740a2ab5dd0023a060bcebe84258ba95bc31949c
Anna Zhykol <annzhy1309@gmail.com>
2023-04-28 15:41:52 +0200

    changes

--- JS/JS20/exercise-6.js
+++ JS/JS20/exercise-6.js
@@ -1,40 +1,40 @@
 class Square {
-    constructor(side) {
-      this.side = side;
-    }
+  constructor(side) {
+    this.side = side;
   }
-  
-  class Rectangle {
-    constructor(width, height) {
-      this.width = width;
-      this.height = height;
-    }
-  }
-  
-  class Circle {
-    constructor(radius) {
-      this.radius = radius;
-    }
-  }
-  
+}
 
-  class AreaCalculator {
-    static calculate (square){
-      return Square.side * Square.side;
-    }
-    static calculate (circle){
-        return Circle.radius * Circle.radius * 3.14;
-    }
-     static calculate (circle){
-        return Circle.radius * Circle.radius * 3.14;
-    }
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
   }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate (square){
+    let squareArea = Square.side * Square.side
+    return squareArea;
+  }
+  static calculate (rectangle){
+    return Rectangle.side * Rectangle.side;
+  }
+  static calculate (circle){
+    return Circle.radius * Circle.radius * 3.14;
+  }
+}
 
 
-  const square = new Square(4);
-  const rectangle = new Rectangle(4, 2);
-  const circle = new Circle(5);
-  
-  console.log(AreaCalculator.calculate(square));
-  console.log(AreaCalculator.calculate(rectangle));
-  console.log(AreaCalculator.calculate(circle));
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
