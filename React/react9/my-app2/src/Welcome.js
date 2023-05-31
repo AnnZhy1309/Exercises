a04091427b02e009384fc6eb226dff85401a120e
Anna Zhykol <annzhy1309@gmail.com>
2023-05-31 20:31:29 +0200

    react9

--- React/react9/my-app2/src/Welcome.js
+++ React/react9/my-app2/src/Welcome.js
@@ -5,9 +5,18 @@ export class Welcome extends React.Component{
     render(){
         return <div>
         <h2>Welcome, {this.props.name}!</h2>
-        <p>Your age is {this.props.age}</p>
+        <Age age={70}/>
         </div>
     }
 }
 
-export const person = <Welcome name="John" age={30}></Welcome>
+export class Age extends React.Component{
+    render()  {
+        return(
+        <div>
+            {this.props.age>18 && this.props.age<65 &&
+            <p>Your age is {this.props.age}</p>}
+
+        </div>
+            )
+}}
