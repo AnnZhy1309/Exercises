408b1b2a72935f7cf7b8c03373322dd0eb55a7a6
Anna Zhykol <annzhy1309@gmail.com>
2023-05-31 21:40:13 +0200

    react12

--- React/react12/my-app2/src/App.js
+++ React/react12/my-app2/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import {Welcome} from "./Welcome";
+import { Counter } from "./Counter";
 
 class App extends React.Component{
   render(){
     return(
+      <div>
       <Welcome name="John" />
+      <Counter />
+      </div>
     )
   }
 }
