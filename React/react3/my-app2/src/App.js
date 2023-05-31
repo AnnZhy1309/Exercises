858583d27c725990fcbac542b223a2727e3aecb0
Anna Zhykol <annzhy1309@gmail.com>
2023-05-31 14:13:50 +0200

    react3

---
+++ React/react3/my-app2/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { greeting} from "./Welcome";
+
+class App extends React.Component{
+    render(){
+      return(
+        greeting
+      )
+    }
+  }
+
+  export default App
