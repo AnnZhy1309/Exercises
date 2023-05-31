3aa0df940addef47729fa95c2550916f7aef60bf
Anna Zhykol <annzhy1309@gmail.com>
2023-05-31 21:30:04 +0200

    react11

--- React/react11/my-app2/src/Welcome.js
+++ React/react11/my-app2/src/Welcome.js
@@ -14,9 +14,9 @@ export class Age extends React.Component{
     render()  {
         return(
         <div>
-            {this.props.age>18 && this.props.age<65
-             &&
-            <p>Your age is {this.props.age}</p>}
+            {this.props.age>18
+               ?<p>Your age is {this.props.age}</p>
+               :<p>You're too young</p>}
         </div>
             )
 }}
