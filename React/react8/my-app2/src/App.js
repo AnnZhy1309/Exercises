f0b6f8bf79b5ac70878e617e529bd0f0bdfd1de2
Anna Zhykol <annzhy1309@gmail.com>
2023-05-31 15:15:21 +0200

    react8

--- React/react8/my-app2/src/App.js
+++ React/react8/my-app2/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {person} from "./Welcome";
+import {Welcome} from "./Welcome";
 
 class App extends React.Component{
-    render(){
-      return(
-        person
-      )
-    }
+  render(){
+    return(
+      <Welcome name="John" />
+    )
   }
+}
 
   export default App
--- React/react8/my-app2/src/Welcome.js
+++ React/react8/my-app2/src/Welcome.js
@@ -5,9 +5,18 @@ export class Welcome extends React.Component{
     render(){
         return <div>
         <h2>Welcome, {this.props.name}!</h2>
-        <p>Your age is {this.props.age}</p>
+        <Age age={15}/>
         </div>
     }
 }
 
-export const person = <Welcome name="John" age={30}></Welcome>
+export class Age extends React.Component{
+    render()  {
+        return(
+        <div>
+            {!!this.props.age  &&
+            <p>Your age is {this.props.age}</p>}
+
+        </div>
+            )
+}}
