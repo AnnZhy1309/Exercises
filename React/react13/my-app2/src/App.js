4694fa589cd996c22d4db48f94e09f19bec76bb8
Anna Zhykol <annzhy1309@gmail.com>
2023-05-31 21:54:09 +0200

    react13

--- React/react13/my-app2/src/App.js
+++ React/react13/my-app2/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import {Welcome} from "./Welcome";
+import { Counter } from "./Counter";
 
 class App extends React.Component{
   render(){
     return(
+      <div>
       <Welcome name="John" />
+      <Counter incrementInterval={2000} incrementValue={10} initialValue={6}/>
+      </div>
     )
   }
 }
