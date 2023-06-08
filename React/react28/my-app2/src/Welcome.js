e7ce3843a10857bf3f4837f716a549bab233efb4
Anna Zhykol <annzhy1309@gmail.com>
2023-06-08 12:08:21 +0200

    react 28 3

--- React/react28/my-app2/src/Welcome.js
+++ React/react28/my-app2/src/Welcome.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { DisplayLanguage } from './LanguageContext';
 
-const String ={
-    en: {welcome_phrase: "Welcome",
-         your_age: "Your age is "},
-    ru: {welcome_phrase: "Добро пожаловать",
-        your_age: "Ваш возраст "}
-}
 
 export class Welcome extends React.Component{
     render(){
-        return <div>
-            <DisplayLanguage.Consumer>
-              {(language)=>{
                 return <div>
-                    <h2>{String[language].welcome_phrase} {this.props.name}!</h2>
-                    <p>{String[language].your_age} {this.props.age}</p>
+                    <h2>Welcome{this.props.name}!</h2>
+                    <p>Your age is{this.props.age}</p>
                 </div>
-              }}
-        </DisplayLanguage.Consumer>
-        </div>
     }
 }
 
