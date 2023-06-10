0a4a574a0c02d633bc40d5faa4e5538bd67692a5
Anna Zhykol <annzhy1309@gmail.com>
2023-06-10 15:13:47 +0200

    custom hooks 1 react36

---
+++ React/react36/my-app2/src/useCounter.js
@@ -0,0 +1,20 @@
+export function useCounter(initialValue=0){
+    const[counter, setCounter] = useState(initialValue)
+
+    function handleCounterIncrement(){
+        setCounter((c)=> c+1)
+    }
+    function handleCounterDecrement(){
+        setCounter((c)=>c-1)
+    }
+    function handleCounterReset(){
+        setCounter(initialValue)
+    }
+
+    return({
+        counter: counter,
+        onIncrement: handleCounterIncrement,
+        onDecremennt: handleCounterDecrement,
+        onReset: handleCounterReset,
+    })
+}
